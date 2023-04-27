90f4d0801495137106337ce39950ad33f4ec9611
PreranaPurkar <preranapurkar12@gmail.com>
2023-04-27 12:57:36 +0530

    [MODIFIED]:[PRERANA] Code Modification with Switch Case

--- src/com/technospace/EmployeeWageComputation/Employee.java
+++ src/com/technospace/EmployeeWageComputation/Employee.java
@@ -18,6 +18,7 @@ System.out.println("Welcome to Employee Wage Computation Program");
 		final int FULL_TIME_HR=8;
 		final int PART_TIME_HR=4;
 		final int IS_PART_TIME=0;
+		final int IS_FULL_TIME=1;
 		
 		
 		//Use Random class for Attendance check
@@ -29,22 +30,29 @@ System.out.println("Welcome to Employee Wage Computation Program");
 		
 		if(attendance == IS_ABSENT) {
 			System.out.println("Employee is Absent");
-		} else {
+			} 
+		else {
 			System.out.println("Employee is Present");
 			empType=random.nextInt(2);
-			if(empType==IS_PART_TIME){
+			//Use Switch case for find the employee is Full time or Part time
+			
+			switch(empType){
+			case IS_PART_TIME:
 				System.out.println("Employee is Part Time");
 				daily_Wage = WAGE_PER_HR * PART_TIME_HR;
-			}
-			else{
+				break;
+				
+			case IS_FULL_TIME:
 				System.out.println("Employee is Full Time");
 				daily_Wage = WAGE_PER_HR * FULL_TIME_HR;
+				break;
 			}
+					
 			}
-			// Find the Daily wage of present Employee
-			
-			
 		System.out.println("Daily Wage= "+daily_Wage);
+			
+		}
+				// Find the Daily wage of present Employee
 	}
 
-}
+
