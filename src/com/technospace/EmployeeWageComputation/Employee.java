283762b5819fb265159c09a453d6a312321dfc08
PreranaPurkar <preranapurkar12@gmail.com>
2023-04-27 12:29:14 +0530

    [MODIFIED]:[PRERANA] Calculate Daily Wage

--- src/com/technospace/EmployeeWageComputation/Employee.java
+++ src/com/technospace/EmployeeWageComputation/Employee.java
@@ -10,7 +10,11 @@ public class Employee {
 System.out.println("Welcome to Employee Wage Computation Program");
 		
 		int attendance ;
+		int daily_Wage=0;
+		
 		final int IS_ABSENT=0;
+		final int WAGE_PER_HR=20;
+		final int FULL_DAY_HR=8;
 		
 		//Use Random class for Attendance check
 		
@@ -23,7 +27,12 @@ System.out.println("Welcome to Employee Wage Computation Program");
 			System.out.println("Employee is Absent");
 		} else {
 			System.out.println("Employee is Present");
+			
+			// Find the Daily wage of present Employee
+			
+			daily_Wage = WAGE_PER_HR * FULL_DAY_HR;
 		}
+		System.out.println("Daily Wage= "+daily_Wage);
 	}
 
 }
