5ec31a371372fcf9ca32edd3378eaa8f427673da
PreranaPurkar <preranapurkar12@gmail.com>
2023-04-27 12:17:38 +0530

    [MODIFIED]:[PRERANA] Check for Employee Attendance

--- src/com/technospace/EmployeeWageComputation/Employee.java
+++ src/com/technospace/EmployeeWageComputation/Employee.java
@@ -1,11 +1,29 @@
 package com.technospace.EmployeeWageComputation;
 
+import java.util.Random;
+
 public class Employee {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		//Welcome message for user
 System.out.println("Welcome to Employee Wage Computation Program");
+		
+		int attendance ;
+		final int IS_ABSENT=0;
+		
+		//Use Random class for Attendance check
+		
+		Random random=new Random();
+		attendance=random.nextInt(2);
+		
+		System.out.println("Attendance=" +attendance);
+		
+		if(attendance == IS_ABSENT) {
+			System.out.println("Employee is Absent");
+		} else {
+			System.out.println("Employee is Present");
+		}
 	}
 
 }
