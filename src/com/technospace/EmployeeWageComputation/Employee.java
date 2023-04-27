6a8560aee5232f869bd35976c0a5623c8320f7ee
PreranaPurkar <preranapurkar12@gmail.com>
2023-04-27 11:54:33 +0530

    [ADDED]:[Prerana] Welcome message for user

---
+++ src/com/technospace/EmployeeWageComputation/Employee.java
@@ -0,0 +1,11 @@
+package com.technospace.EmployeeWageComputation;
+
+public class Employee {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Welcome message for user
+System.out.println("Welcome to Employee Wage Computation Program");
+	}
+
+}
